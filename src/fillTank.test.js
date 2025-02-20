2d2009e6899f5aa94697e894c2b98cd209610295
Nastya <anastasiia.y.sitnikova@gmail.com>
2025-02-20 15:45:29 +0100

    add solution

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -1,11 +1,105 @@
 'use strict';
 
 describe('fillTank', () => {
-  // const { fillTank } = require('./fillTank');
+  const { fillTank } = require('./fillTank');
 
-  it('should ', () => {
+  let customer;
 
+  beforeEach(() => {
+    customer = {
+      money: 300,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
   });
 
-  // write tests here
+  it('should be declared', () => {
+    expect(fillTank).toBeInstanceOf(Function);
+  });
+
+  it(`should fill full tank if the 'amount' is not given`, () => {
+    fillTank(customer, 2);
+
+    expect(customer).toEqual({
+      money: 236,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 40,
+      },
+    });
+  });
+
+  it('should fill up to maxTankCapacity if amount > maxTankCapacity', () => {
+    fillTank(customer, 2, 40);
+
+    expect(customer).toEqual({
+      money: 236,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 40,
+      },
+    });
+  });
+
+  it('should only fill the amount the customer can pay', () => {
+    fillTank(customer, 12, 30);
+
+    expect(customer).toEqual({
+      money: 0,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 33,
+      },
+    });
+  });
+
+  it('should round the poured amount to the tenth part', () => {
+    fillTank(customer, 3.74, 11);
+
+    expect(customer).toEqual({
+      money: 258.86,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 19,
+      },
+    });
+  });
+
+  it('should round the price to the nearest hundredth part', () => {
+    fillTank(customer, 2.356, 11);
+
+    expect(customer).toEqual({
+      money: 274.08,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 19,
+      },
+    });
+  });
+
+  it('should not pour if amount < 2 liters', () => {
+    fillTank(customer, 3, 1.8);
+
+    expect(customer).toEqual({
+      money: 300,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    });
+  });
+
+  it('should not overflow the tank', () => {
+    fillTank(customer, 3, 40);
+
+    expect(customer).toEqual({
+      money: 204,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 40,
+      },
+    });
+  });
 });
